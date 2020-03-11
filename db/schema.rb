615439c6f79b967d8195897066c46c9e9c29ea90
Julio Aguiar <jaguiarinfo@gmail.com>
2020-03-11 20:07:49 -0300

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_09_204506) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.boolean "admin"
     t.string "name"
