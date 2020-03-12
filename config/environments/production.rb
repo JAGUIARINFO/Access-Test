70cc5a9f60e95a61822ed8851cec3036d930fa3f
Julio Aguiar <jaguiarinfo@gmail.com>
2020-03-12 18:00:31 -0300

    atualiza

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,13 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "testeab116",
+    password: "teste2020"}
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
